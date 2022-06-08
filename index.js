5837e7156388ec175e7467cd3402d35f9fce0333
Jobsidney <jobsidney67@gmail.com>
2022-06-08 08:16:06 +0300

    assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+function returnFirstTwoDrivers(drivers){
+    const neww=drivers.slice(0,2).map(x=>x);
+    return neww;
+}
+function returnLastTwoDrivers(drivers) {
+    const last =drivers.slice(2).map(x=>x)
+    return last;
+}
+
+function returnLastTwoDrivers(){
+
+}
+function createFareMultiplier(fare){
+    return function(){return fare*fare};
+}
+const fareDoubler=fare=>fare*2;
+const fareTripler=fare=>fare*3;
+function selectDifferentDrivers(drivers, callFunc){
+    let drive;
+    if (callFunc===returnFirstTwoDrivers()){
+        returnFirstTwoDrivers(drivers);
+    }else if(callFunc===returnLastTwoDrivers()){
+        returnLastTwoDrivers(drivers);
+    }
+}
